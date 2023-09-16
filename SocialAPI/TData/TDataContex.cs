6319c52fed1b0d85167114c7e969bd1299743628
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2023-09-17 01:28:02 +0600

    Creating Skeleton

---
+++ SocialAPI/TData/TDataContex.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialAPI.TEntities;
+
+namespace SocialAPI.TData
+{
+    public class TDataContex : DbContext
+    {
+        public TDataContex(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
