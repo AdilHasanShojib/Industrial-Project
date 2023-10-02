6ba7ae6881622e9062c3778c7b944a165829c98b
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2023-10-02 12:46:25 +0600

    Frontend Login and Registration Implementation Part-4

--- SocialAPI/TData/Migrations/20231002062016_UserExtension.Designer.cs
+++ SocialAPI/TData/Migrations/20231002062016_UserExtension.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SocialAPI.TData;
 
@@ -11,9 +12,11 @@ using SocialAPI.TData;
 namespace SocialAPI.TData.Migrations
 {
     [DbContext(typeof(TDataContex))]
-    partial class TDataContexModelSnapshot : ModelSnapshot
+    [Migration("20231002062016_UserExtension")]
+    partial class UserExtension
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- SocialAPI/TEntities/AppUser.cs
+++ SocialAPI/TEntities/AppUser.cs
@@ -6,8 +6,45 @@
       public string Name { get; set; }
 
       public byte[] PasswordHash { get; set; }
-        public object passwordhash { get; internal set; }
-        public byte[] PasswordSalt {  get; set; }
-        public object passwordsalt { get; internal set; }
+       
+      public byte[] PasswordSalt {  get; set; }
+
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        private DateTime _dateofBirth;
+        
+        public DateTime DateOfBirth { 
+            
+            get => _dateofBirth; 
+            
+            set => _dateofBirth=value.ToUniversalTime(); }
+
+
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string KnownAs { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
